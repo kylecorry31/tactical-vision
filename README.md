fb893eb036168abc29162ff6e694e81a789c722e
Kyle Corry <kylecorry31@gmail.com>
2020-06-27 14:22:57 +0100

    Remove opencv dependency

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://www.buydisplay.com/full-color-1-5-inch-arduino-raspberry-pi-oled-display
 1. Wire up the OLED as described in the [wiring PDF](https://www.buydisplay.com/full-color-1-5-inch-arduino-raspberry-pi-oled-display-module-128x128)
 2. Install the [bcm2835](https://www.airspayce.com/mikem/bcm2835/) library
 3. Clone this repo and run `make` from the repo's root directory
-4. Install OpenCV (and OpenCV python), picamera, and numpy
+4. Install picamera and numpy
 5. Run the following programs for infrared vision
 `
 python3 ir_camera.py
--- ir_camera.py
+++ ir_camera.py
@@ -4,40 +4,21 @@ import io
 import time
 import numpy as np
 from threading import Condition
-import cv2
-
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
 
 with picamera.PiCamera(resolution=(128, 128), framerate=60) as camera:
     camera.rotation = 180
+    camera.video_denoise = False
     camera.hflip = True
-    stream = StreamingOutput()
+    camera.color_effects = (128, 128)
     last_time = time.time()
-    camera.start_recording(stream, format='mjpeg', quality=100)
-    try:
-        while True:
-            with stream.condition:
-                stream.condition.wait()
-                frame = stream.frame
-                npframe = np.frombuffer(frame, dtype=np.uint8)
-                if len(npframe):
-                    pil_frame = cv2.imdecode(npframe, cv2.IMREAD_COLOR)
-                    pb = pil_frame.flatten()
-                    pb.tofile('infrared')
-                    last_time = time.time()
-    finally:
-        pass
-
+    stream = io.BytesIO()
+    for _ in camera.capture_continuous(stream, 'rgb', use_video_port=True):
+        try:
+            stream.seek(0)
+            frame = stream.read()
+            img = np.frombuffer(frame, dtype=np.uint8)
+            img.tofile('infrared')
+            stream.seek(0)
+            stream.truncate()
+        except:
+            print('Could not process image')
